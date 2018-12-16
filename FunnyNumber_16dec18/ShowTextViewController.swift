6f7160f2e7f812084ae2af75807b11f4026e1a7c
masterUNG <phrombutr@gmail.com>
2018-12-16 15:01:01 +0700

    Create ViewController Success

---
+++ FunnyNumber_16dec18/ShowTextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowTextViewController.swift
+//  FunnyNumber_16dec18
+//
+//  Created by MasterUNG on 16/12/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ShowTextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
